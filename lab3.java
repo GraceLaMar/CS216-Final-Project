2c48c971debc40f2046d5f55c7f12416b4e92d39
GraceLaMar <lamarg@huntington.edu>
2025-09-08 00:30:29 -0400

    Create lab3.java

---
+++ lab3.java
@@ -0,0 +1,56 @@
+// lab3.java
+// grace lamar
+// fall 2025
+// power and GCD recursive functions
+//
+public class Main {
+    public static void main(String[] args) {
+
+        // call to power function
+        System.out.println(power(2, 3));
+        System.out.println();
+
+        System.out.println(power(4,6));
+        System.out.println();
+
+        System.out.println(power(8,7));
+        System.out.println();
+
+        System.out.println(power(9,6));
+        System.out.println();
+
+        
+        // call to gcd function
+        System.out.println(gcd(10, 45));
+        System.out.println();
+
+        System.out.println(gcd(12,72));
+        System.out.println();
+
+        System.out.println(gcd(20,65));
+        System.out.println();
+
+        System.out.println(gcd(16,52));
+        System.out.println();
+
+    }
+
+    public static int power(int x, int y) {
+        System.out.println("power(" + x + "," + y + ")");
+        if (y == 0)
+            return 1;
+        else if (y == 1)
+            return x;
+        else
+            return x * power(x, y - 1);
+    }
+
+    public static int gcd(int x, int y) {
+        System.out.println("gcd(" + x + "," + y + ")");
+        if (y == 0)
+            return x;
+        else
+            return gcd(y, x % y);
+    }
+
+}
